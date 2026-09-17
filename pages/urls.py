90511ed591e9cb9fc8286cdf7bc9db99c0a5606b
jeylofton <jeylofton@gmail.com>
2026-09-17 08:18:17 -0500

    urls update only

--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,5 @@ from . import views
 # The name= is a nickname you can use in templates and code.
 urlpatterns = [
     path("", views.home, name="home"),          # http://127.0.0.1:8000/
-    path("about/", views.about, name="about"),  # http://127.0.0.1:8000/about/
+    path("", views.about, name="about"),  # http://127.0.0.1:8000/about/
 ]
