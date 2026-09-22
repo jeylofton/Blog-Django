0cf2c86d2161783ff49275425d133ebd2221c1fa
jeylofton <jeylofton@gmail.com>
2026-09-22 16:44:43 -0500

    Assignment 2 Complete

--- pages/views.py
+++ pages/views.py
@@ -7,5 +7,12 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = "pagesTemplates/home.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = "Jey"
+        print(context)
+        return context
+
+
 class AboutPageView(TemplateView):
     template_name = "pagesTemplates/about.html"
