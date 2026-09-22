b6be46203b2b51a8c234061b8b5deee1d71d40c9
jeylofton <jeylofton@gmail.com>
2026-09-22 18:39:57 -0500

    End of Class Update

--- pages/views.py
+++ pages/views.py
@@ -21,4 +21,10 @@ def contact_page(request):
     # print(request.__dict__)
     # return HttpResponse("Hello World from a FBV")
 
-    return render(request, "pagesTemplates/contact.html")
+    contact_info ={
+        "name": "Jey Lofton",
+        "address": "Somewhere 125 PAN",
+        "email": "loftonjey9@mail.com"
+    }
+
+    return render(request, "pagesTemplates/contact.html", contact_info)
