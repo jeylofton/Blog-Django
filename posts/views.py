2d961a8f7cb8232e14c2ca7ea4d1d1d41bd35c25
jeylofton <jeylofton@gmail.com>
2026-09-23 20:34:58 -0500

    Class Break Update

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.urls import reverse_lazy
+from .models import Post
+from django.contrib.auth.models import User
 
 # Create your views here.
+class PostListView(ListView):
+    template_name = "postsTemplates/list.html"
+
+    model = Post
+
+    context_object_name = "posts"
+
+class PostDetailView(DetailView):
+    template_name = "poststemplates/detail.html"
+    model = Post
+    context_object_name = "single_post"
