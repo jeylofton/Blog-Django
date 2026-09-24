d18e6499d59bfd0d0bacb245b4a54763bca399d9
jeylofton <jeylofton@gmail.com>
2026-09-23 22:21:07 -0500

    End fo Class Code Update

--- posts/views.py
+++ posts/views.py
@@ -21,3 +21,22 @@ class PostDetailView(DetailView):
     template_name = "poststemplates/detail.html"
     model = Post
     context_object_name = "single_post"
+
+class PostCreateView(CreateView):
+        template_name = "postsTemplates/new.html"
+        model = Post
+        fields = ["title", 'subtitle', "body"]
+
+        def form_valid(self, form):
+            form.instance.author = User.objects.last()
+            return super().form_valid(form)
+
+class PostUpdateView(UpdateView):
+    template_name = "postsTemplates/edit.html"
+    model = Post
+    fields =["title", "subtitle", "body"]
+
+class PostDeleteView(DeleteView):
+    template_name = "postsTemplates/delete.html"
+    model = Post
+    success_url = reverse_lazy("post_list")
